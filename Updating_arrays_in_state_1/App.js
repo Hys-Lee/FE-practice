5e7e6e85c87b4cc07e632983c61bbab1d02e632e
Hys-Lee <dltkdgus3275@gmail.com>
2023-07-05 12:48:26 +0000

    sovled

---
+++ Updating_arrays_in_state_1/App.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const initialProducts = [
+  {
+    id: 0,
+    name: "Baklava",
+    count: 1
+  },
+  {
+    id: 1,
+    name: "Cheese",
+    count: 5
+  },
+  {
+    id: 2,
+    name: "Spaghetti",
+    count: 2
+  }
+];
+
+export default function ShoppingCart() {
+  const [products, setProducts] = useState(initialProducts);
+
+  function handleIncreaseClick(productId) {
+    setProducts(
+      products.map((content) => {
+        if (content.id === productId) {
+          return { ...content, count: content.count + 1 };
+        } else {
+          return { ...content };
+        }
+        // console.log(content.id);
+        // return { ...content, id: productId };
+      })
+    );
+  }
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          {product.name} (<b>{product.count}</b>)
+          <button
+            onClick={() => {
+              handleIncreaseClick(product.id);
+            }}
+          >
+            +
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
